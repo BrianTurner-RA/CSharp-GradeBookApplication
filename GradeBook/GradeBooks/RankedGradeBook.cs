bf6f39e0e6931c593b8f9c822e1aae2019a539b4
Brian Turner <bgtsoft@outlook.com>
2018-12-06 10:01:48 +0000

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -36,5 +36,14 @@ namespace GradeBook.GradeBooks
 
             return grades[threshold * 4 - 1] <= averageGrade ? 'D' : 'F';
         }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+            base.CalculateStatistics();
+        }
     }
 }
